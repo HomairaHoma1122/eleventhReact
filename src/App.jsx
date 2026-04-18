9699828b6bdc69c3953d498021d65572f8ec2ef7
HomairaHoma1122 <homaira.amini22@gmial.com>
2026-04-18 10:25:24 -0700

    Adding some else hokes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
 import { useEffect, useState } from "react";
 import Testing from "./My";
+import { Cloud, Sun } from "lucide-react";
 
 function App() {
   const [weatherDt, getdt] = useState(null);
+
+
+  const [hour, setHours]= useState("");
+  const [minute, setMinutes] = useState("");
+  const [seconds, setSeconds] = useState("");
+
+
+const [sunHour, setSunRHours]= useState("");
+  const [sunMinute, setSunRMinutes] = useState("");
+  const [sunriseSeconds, setSunRSeconds] = useState("");
+
   useEffect(()=>{
     async function getWeather(){
-      const weather = await fetch("https://api.openweathermap.org/data/2.5/weather?q=Ghazni&appid=684418efa9274f3a6491868b0271123");
+      const weather = await fetch("https://api.openweathermap.org/data/2.5/weather?q=Ghazni&unit=metric&appid=982b6b9545eeff845233ab0fb88f85dc");
        const natija = await weather.json();
        getdt(natija);
+       const sunRiseDate = new Date(natija.sys.sunrise * 1000);
+       const sunRHour = sunRiseDate.setHours();
+       const sunRMinut = sunRiseDate.setMinutes();
+       const sunRSecond = sunRiseDate.setSeconds();
+       setHours(sunRHour);
+       setMinutes(sunRMinut);
+       setSeconds(sunRSecond);
+
+       sunSetDate = new Date(natija.sys.sunset*1000);
+       const sunHour = sunSetDate.setSunRHours();
+       const sunMinute = sunSetDate.setSunRMinutes();
+       const sunriseSeconds = sunSetDate.setSunRSeconds();
+       setSunRHours(sunHour);
+       setSunRMinutes(sunMinute);
+       setSunRSeconds(sunRMinute)
     }
     getWeather();
   }, []);
@@ -37,24 +64,80 @@ function App() {
 
 
         width: "50%",
-        height: "60vh",
+        height: "40vh",
         padding: "20px",
-        border: "2px dotted blue",
+        border: "2px solid black",
         borderRadius: "12px",
         justifyContent: "center",
         TextItems: "center",
         margin: "32px auto"
       }}>
-        {weatherDt.weather[0].main ==="Clear" ? <Sun />  : weatherDt.weater[0].main === ""}
+        
       <h1 
       style={{
-                fontSize: "40px",
+                fontSize: "20px",
                 textAlign: "center",
                 color: "green"
             }}
-      >In the name of Allah</h1>
+      >Information of Weather</h1>
+
+          <div style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          
+            color: "gray"
+          }}>
+        {weatherDt.weather[0].main ==="Clear" ? (<Sun />) 
+        : weatherDt.weather[0].main === "Clouds" ? (<Cloud />)
+        : weatherDt.weather[0].main === "rain" ? (<Rain />) :
+        weatherDt.weather[0].main
+      }
+      </div>
+      <div style={{
+        display: "flex",
+        width: "100%",
+        justifyContent: "space-between",
+        alignItems: "center"
+      }}>
+        
+        <p style={{
+          display: "flex",
+          gap: "5px"
+        }}>
+          <span>Temprature</span>
+          <span>{weatherDt.main.temp}</span>
+        </p>
+        <p style={{
+          display: "flex",
+          gap: "5px"
+        }}>
+          <span>Humidity</span>
+          <span>{weatherDt.main.humidity} %</span>
+        </p>
+      </div>
 
 
+      <div style={{
+        display: "flex",
+        width: "100%",
+        justifyContent: "space-between",
+        alignItems: "center"
+      }}>
+        
+        <p style={{
+          display: "flex",
+          gap: "12px"
+        }}>
+          <span>Sunset</span>
+          <span>{hour}:{minute}:{seconds}</span>
+        </p>
+
+        <p>
+          <span>Sunrise</span>
+          <span>{sunHour}:{sunMinute}:{sunriseSeconds}</span>
+        </p>
+      </div>
       </div>
     </div>
   )
